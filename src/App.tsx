1ad5c4f755b07a7fd40d614b3eafb77494c569ab
truejojo <johannesveh@yahoo.de>
2023-01-27 13:19:55 +0100

    functionality for list-item with delete btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,45 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-interface ListItemProps {
-  title: string;
-  url: string;
-  author: string;
-  num_comments: number;
-  points: number;
-  objectID: number;
-}
+const initialState: ListItemProps[] = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
-interface ListProps {
-  list: ListItemProps[];
-}
+const useStorageState = (
+  key: string,
+  initialState: string
+): [string, (newValue: string) => void] => {
+  const [value, setValue] = useState(localStorage.getItem(key) || initialState);
+
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
 
 const App = () => {
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-  const [searchTerm, setSearchTerm] = useState("");
+  const [stories, setStories] = useState<ListItemProps[]>(initialState);
+  const [searchTerm, setSearchTerm] = useStorageState("search", "");
+
+  const removeItem = (id: number) =>
+    setStories((prevStories) =>
+      prevStories.filter((story) => story.objectID !== id)
+    );
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) =>
     setSearchTerm(event.target.value);
@@ -43,43 +50,104 @@ const App = () => {
 
   return (
     <div>
-      <h1>Hello World!</h1>
-      <Search search={searchTerm} onSearch={handleSearch} />
-      <h2>My List</h2>
-      {searchTerm.length > 0 && <List list={searchStories} />}
+      <h1>New Hacker Stories</h1>
+
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
+
+      {searchTerm.length > 0 && (
+        <List list={searchStories} removeItem={removeItem} />
+      )}
     </div>
   );
 };
 
-interface SearchProps {
-  search: string;
-  onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
+interface IInputWithLabelProps {
+  id: string;
+  value: string;
+  type?: string;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  isFocused: boolean;
+  children: React.ReactNode;
 }
-const Search = ({ search, onSearch }: SearchProps) => (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" value={search} onChange={onSearch} />
-  </div>
-);
 
-const List = ({ list }: ListProps) => (
+const InputWithLabel = ({
+  id,
+  value,
+  type = "text",
+  onInputChange,
+  isFocused,
+  children,
+}: IInputWithLabelProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
+
+interface ListProps {
+  list: ListItemProps[];
+  removeItem: (id: number) => void;
+}
+const List = ({ list, removeItem }: ListProps) => (
   <ul>
-    {list.map(( item ) => (
-      <Item key={item.objectID} {...item} />
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} removeItem={removeItem} />
     ))}
   </ul>
 );
 
-const Item = ({ url, title, author, num_comments, points }: ListItemProps) => (
-  <li>
-    <h3>
-      <a href={url}>{title}</a>
-    </h3>
-    <h4>{author}</h4>
-    <p>{num_comments}</p>
-    <p>{points}</p>
-    <hr />
-  </li>
-);
+interface ListItemProps {
+  title: string;
+  url: string;
+  author: string;
+  num_comments: number;
+  points: number;
+  objectID: number;
+}
+
+interface ItemProps {
+  item: ListItemProps;
+  removeItem: (id: number) => void;
+}
+
+const Item = ({ item, removeItem }: ItemProps) => {
+  return (
+    <li>
+      <h3>
+        <a href={item.url}>{item.title}</a>
+      </h3>
+      <h4>{item.author}</h4>
+      <p>{item.num_comments}</p>
+      <p>{item.points}</p>
+      <button type="button" onClick={() => removeItem(item.objectID)}>
+        Delete
+      </button>
+      <hr />
+    </li>
+  );
+};
 
 export default App;
